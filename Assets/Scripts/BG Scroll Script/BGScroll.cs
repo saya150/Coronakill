12b2b85b80f3a532042d15c1caaa93249fe4437d
Syedtoufiq <syed.toufiq99@gmail.com>
2021-04-04 22:45:45 +0530

    initial

---
+++ Assets/Scripts/BG Scroll Script/BGScroll.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroll : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float scroll_speed = 0.1f;
+    private MeshRenderer mesh_Renderer;
+    private float x_Scroll;
+
+
+    void Awake()
+    {
+        mesh_Renderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Scroll();
+        
+    }
+    void Scroll()
+    {
+        x_Scroll = Time.time * scroll_speed;
+        Vector2 offset = new Vector2(x_Scroll, 0f);
+        mesh_Renderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
+
+    }
+}
